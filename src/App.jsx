81867331e1ba461722b6c3d58b190175173cce50
Samarion Junior <samarionbackup471@gmail.com>
2025-06-04 14:38:59 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home"
 
 import "./style.css"
 
 const App = _ => {
   return (
-    <Routes>
-      <Route exact path="/Home" element={<Home />} />
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="*" element={<Home />} />
-    </Routes>
+    <Home />
   );
 }
 
